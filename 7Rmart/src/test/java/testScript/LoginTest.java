d3e9027d8a6c45ddc67b510286fce86140557bcd
91799 <91799@192.168.1.39>
2025-01-21 20:20:33 +0530

    7Rmart

--- 7Rmart/src/test/java/testScript/LoginTest.java
+++ 7Rmart/src/test/java/testScript/LoginTest.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import Pages.LoginPage;
